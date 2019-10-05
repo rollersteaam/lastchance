594e5c87464f3dba32d43f2858c3a0233fca6b19
rollersteaam <rollersteaam@googlemail.com>
2019-10-05 18:57:16 +0100

    Make enemy initially evolve

---
+++ LastChance/Assets/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Makes the camera follow a target. By default, it follows the player.
+/// </summary>
+public class CameraFollow : MonoBehaviour
+{
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        
+    }
+}
