bf05fdc483ee7f31bef4eca1dc0ee67b987915d5
rollersteaam <rollersteaam@googlemail.com>
2019-10-05 12:55:59 +0100

    Add attack input

--- LastChance/Assets/Scripts/GameScripts/Gameplay/Actors/CharacterControl.cs
+++ LastChance/Assets/Scripts/GameScripts/Gameplay/Actors/CharacterControl.cs
@@ -1,33 +1,47 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// Binds input to an object that implements <see cref="IMoveable"/>.
+/// Binds input to character modules.
 /// </summary>
 public class CharacterControl : MonoBehaviour
 {
     IMoveable moveable;
+    ICharacterAnimator animator;
 
     void Start()
     {
         moveable = GetComponent<IMoveable>();
+        animator = GetComponent<ICharacterAnimator>();
     }
 
     void FixedUpdate()
     {
         ProcessMovementInput();
+        ProcessAttackInput();
     }
 
     /// <summary>
-    /// Moves object based on horizontal and vertical axes.
+    /// Makes object attack based on Fire1 input axis.
+    /// </summary>
+    void ProcessAttackInput()
+    {
+        if (Input.GetAxis("Fire1") == 0) return;
+
+        animator.Attack();
+    }
+
+    /// <summary>
+    /// Moves object based on Horizontal and Vertical input axes.
     /// </summary>
     void ProcessMovementInput() {
-        moveable.Move(
-            new Vector2(
-                Input.GetAxis("Horizontal"),
-                Input.GetAxis("Vertical")
-            )
-        );
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        if (h == 0 || v == 0) return;
+
+        moveable.Move(new Vector2(h, v));
     }
 }
