6c9c67680d8614613804ee2a1407dec5dd8f02b2
rollersteaam <rollersteaam@googlemail.com>
2019-10-05 19:02:33 +0100

    Create camera follow

--- LastChance/Assets/CameraFollow.cs
+++ LastChance/Assets/CameraFollow.cs
@@ -7,12 +7,26 @@ using UnityEngine;
 /// </summary>
 public class CameraFollow : MonoBehaviour
 {
+    Transform followedTransform;
+
     /// <summary>
     /// Start is called on the frame when a script is enabled just before
     /// any of the Update methods is called the first time.
     /// </summary>
     void Start()
     {
-        
+        followedTransform = GameObject.FindWithTag("Player").transform;
+    }
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        transform.position = new Vector3(
+            followedTransform.position.x,
+            followedTransform.position.y,
+            transform.position.z
+        );
     }
 }
