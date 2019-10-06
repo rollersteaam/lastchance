a362690eae022ca0cb35a7e3263d842f87888de8
rollersteaam <rollersteaam@googlemail.com>
2019-10-06 20:02:16 +0100

    Add sticky background

---
+++ LastChance/Assets/Background.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    void FixedUpdate()
+    {        
+        var pos = Camera.main.transform.position;
+        transform.position = new Vector3(
+            pos.x,
+            pos.y,
+            transform.position.z
+        );
+    }
+
+    void Update()
+    {
+        var amount = Camera.main.transform.position.z / -20;
+        transform.localScale = new Vector3(
+            amount,
+            amount,
+            amount
+        );
+    }
+}
